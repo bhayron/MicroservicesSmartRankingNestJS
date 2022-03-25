a8074767bc0d453bba5143dea42e09a9a8885fd5
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-03-25 00:34:01 -0300

    RMQ

--- api-gatway/src/app.controller.ts
+++ api-gatway/src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private logger = new Logger(AppController.name);
+  private clientAdminBackend: ClientProxy;
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://user:qdWT3LNwIBai@100.26.36.255:5672/smartranking'],
+        queue: 'admin-backend',
+      },
+    });
+  }
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() CriarCategoriaDto: CriarCategoriaDto) {
+    return await this.clientAdminBackend.emit(
+      'criar-categoria',
+      CriarCategoriaDto,
+    );
   }
 }
