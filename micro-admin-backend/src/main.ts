901bb91c18b47ffc9121f5903b671fb5370d1aa6
bhayron <bhayronklivilan@gmail.com>
2022-04-12 09:58:10 -0300

    dotenv

--- micro-admin-backend/src/main.ts
+++ micro-admin-backend/src/main.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.URL_RMQ],
+      queue: 'admin-backend',
+    },
+  });
+
+  await app.listen(() => logger.log('Microservice is listening'));
 }
 bootstrap();
